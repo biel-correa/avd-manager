59dc88c6051e170cab40507aaa986f2d12b40447
biel-correa <gabriel@correa.dev.br>
2024-03-24 11:06:50 -0300

    feature: add app bar

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:avd_manager/pages/home/android_home_not_set.dart';
 import 'package:avd_manager/pages/home/list_android_devices.dart';
 import 'package:avd_manager/services/emulator_service.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -23,19 +24,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('AVD Manager'),
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.bug_report),
+            color: Colors.red,
+            onPressed: _reportBug,
+          ),
+        ],
+      ),
       body: _isAndroidHomeSet
           ? const ListAndroidDevices()
           : AndroidHomeNotSet(retry: _retry),
     );
   }
 
-  Future<void> _debug() async {
-    await EmulatorService().listAvds();
-  }
-
   void _retry() {
     setState(() {
       _isAndroidHomeSet = EmulatorService().isAndroidHomeSet();
     });
+  }
+
+  void _reportBug() {
+    Uri uri = Uri.https('github.com', 'biel-correa/avd_manager/issues');
+
+    launchUrl(uri);
   }
 }
