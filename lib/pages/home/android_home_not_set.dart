d4c464e5aeab234cbb449ad02aad5ff40d597cee
Gabriel Corrêa <gcorrea@bocavox.com>
2024-03-25 17:31:04 -0300

    feature: improve android home instructions

--- lib/pages/home/android_home_not_set.dart
+++ lib/pages/home/android_home_not_set.dart
@@ -12,11 +12,28 @@ class AndroidHomeNotSet extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Android Home is not set'),
-          const SizedBox(height: 16),
-          ElevatedButton(onPressed: _redirect, child: const Text('Learn more')),
+          const Text(
+            'ANDROID_HOME is not set',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+          ),
           const SizedBox(height: 6),
-          TextButton(onPressed: retry, child: const Text('Retry')),
+          const Text(
+              'ANDROID_HOME is an environment variable that points to the Android SDK folder. It is required to run AVD Manager.'),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _redirect,
+                child: const Text('Learn more'),
+              ),
+              const SizedBox(width: 6),
+              TextButton(
+                onPressed: retry,
+                child: const Text('Retry'),
+              ),
+            ],
+          )
         ],
       ),
     );
