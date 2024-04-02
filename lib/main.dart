4995f4dcce956881ac1b057ee5016f8263ee4b88
biel-correa <gabriel@correa.dev.br>
2024-04-02 20:51:05 -0300

    feature: add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:avd_manager/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://caf545efdce7450bc7dc50979570fada@o4507000198397952.ingest.us.sentry.io/4507000211898368';
+      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+      // We recommend adjusting this value in production.
+      options.tracesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(MyApp()),
+  );
 }
 
 class MyApp extends StatefulWidget {
